0c6b15acc4d4f0b7e82d2d3985685c8929b493d7
053alex1 <49307396+053alex1@users.noreply.github.com>
2019-04-13 23:09:19 +0200

    Movement of the character

---
+++ Greedy-ForumCollect/Assets/Scripts/Character_mov.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character_mov : MonoBehaviour {
+
+    private Vector2 direction = Vector2.zero;
+
+    public float RunSpeed = 0.5f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Tecla();
+
+        Orientation();
+
+        Move();
+    }
+
+    void Tecla()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+
+        else if(Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.localPosition += (Vector3)(direction * RunSpeed) * Time.deltaTime;
+        direction = Vector2.zero;
+    }
+
+    void Orientation()
+    {
+        if(direction == Vector2.right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        
+        else if(direction == Vector2.left)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+}
