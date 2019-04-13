220f7c1812f1d0d3c01874ac8f851dfdf4ee41f2
A.Tauste <taustano23@gmail.com>
2019-04-13 19:51:21 +0200

    CountDownTimer(1º Version)
    Added first CountdownTimer version.

---
+++ Greedy-ForumCollect/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float startingTime = 10f;
+    float currentTime = 0f;
+    [SerializeField] Text countdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime >= 0)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = "Tiempo: " + currentTime.ToString("0.00");
+        }
+        else {
+            countdownText.text = "FIN!";
+        }
+    }
+}
