74588c3021dee7765df698b8194d1cb386a5efa5
Moisés <moiminme@inf.upv.es>
2019-04-16 17:17:41 +0200

    Enemy movement v1
    Primera versión inacabada del movimiento del enemigo

---
+++ Greedy-ForumCollect/Assets/Scripts/enemy_ia.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_ia : MonoBehaviour{
+
+    private float temporizadorUltimaDireccion;
+    private readonly float temporizadorCambioDeDireccion = 3f;
+    private Vector2 direccion = Vector2.zero;
+    private Vector2 movimientoPorSegundo;
+
+    public float runSpeed = 0.9f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        temporizadorUltimaDireccion = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - temporizadorUltimaDireccion > temporizadorCambioDeDireccion){
+            
+            temporizadorUltimaDireccion = Time.time;
+            ChooseNextMove();
+        }
+        
+        Move();
+        Orientation();
+        
+    }
+
+    
+
+    void ChooseNextMove()
+    {
+
+     direccion = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+     movimientoPorSegundo = direccion * runSpeed;  
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = new Vector2(transform.position.x + (movimientoPorSegundo.x * Time.deltaTime), 
+        transform.position.y + (movimientoPorSegundo.y * Time.deltaTime));
+    }
+
+
+    void Orientation()
+    {
+        if(direccion == Vector2.right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        
+        else if(direccion == Vector2.left)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+}
