251615bb92ab5ed14b1a02a1b584e29c5109eb8a
053alex1 <053alex@gmail.com>
2019-05-13 00:09:04 +0200

    powers

--- Greedy-ForumCollect/Assets/Scripts/Powers.cs
+++ Greedy-ForumCollect/Assets/Scripts/Powers.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Powers
+public class Powers : Interactable
 {
     public bool invencibility = false;
     public bool superSpeed = false;
     public bool invisibility = false;
+
+    public Character_mov Player;
 
     public void StartInvulnerable()
     {
@@ -22,11 +24,29 @@ public class Powers
     {
         superSpeed = true;
         return 0.2f;
+
+        //Player.RunSpeed += 0.2f;
+        //return 0.2f;
     }
 
     public float StopSuperSpeed()
     {
         superSpeed = false;
         return -0.2f;
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    public void PickUp()
+    {
+        Debug.Log("Picking up item");
+        //Instantiate(effect, transform.position, Quaternion.identity);
+        StartSuperSpeed();
+        Destroy(gameObject);
     }
 }
