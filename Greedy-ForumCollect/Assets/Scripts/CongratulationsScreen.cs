6c9426fe11a6eab7c80a084932493687d1e1a680
A.Tauste <taustano23@gmail.com>
2019-05-13 19:33:16 +0200

    Game passed screen (not finished)

---
+++ Greedy-ForumCollect/Assets/Scripts/CongratulationsScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CongratulationsScreen : MonoBehaviour
+{
+
+    public void finishGame() {
+        //To be finished
+        Debug.Log("Congratulations, you finished the game. Coming back to main menu...");
+        //Save score...
+        SceneManager.LoadScene("MainMenu");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
