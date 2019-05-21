c9baf1821b43862108f6a396408f13e057516d0f
053alex1 <053alex@gmail.com>
2019-05-21 22:59:09 +0200

    generator

--- Greedy-ForumCollect/Assets/Scripts/Generator.cs
+++ Greedy-ForumCollect/Assets/Scripts/Generator.cs
@@ -6,24 +6,44 @@ public class Generator : MonoBehaviour
 {
     public GameObject Boots;
     public GameObject Shield;
+    public GameObject Food;
+
+    public GameObject Ajustador;
 
     public CountDownTimer SpawnTime;
 
     public Node[] Nodes;
 
+    public int frutas;
+    private int i = 0;
+    private int node = 0;
+    
+
     void Start()
     {
-        
+        while(i != frutas)
+        {
+            do
+            {
+                node = Random.Range(0, 65);
+            } while (Nodes[node].GetBusy());
+            
+            Generate("Food", node);
+            i++;
+        }
     }
 
     void Update()
     {
-        if(SpawnTime.getTime() % 15f == 0f)
+        if(SpawnTime.getTime() % 15 == 0)
         {
             int type = Random.Range(0, 1);
-            int node = Random.Range(0, 65);
+            do
+            {
+                node = Random.Range(0, 65);
+            } while (Nodes[node].GetBusy());
 
-            switch(type)
+            switch (type)
             {
                 case 0: Generate("Boots", node);
                     break;
@@ -31,18 +51,26 @@ public class Generator : MonoBehaviour
                     break;
             }
         }
-
-        else if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Generate("Boots", 3);
-        }
     }
 
     void Generate(string name, int pos_node)
     {
         if(name == "Boots")
         {
-             Instantiate(Boots, Nodes[pos_node].transform.position, Nodes[pos_node].transform.rotation);
+            Instantiate(Boots, ((Vector3) Nodes[pos_node].Position) + Ajustador.transform.position, Nodes[pos_node].transform.rotation);
+            Nodes[pos_node].ToBusy();
+        }
+
+        else if(name == "Shield")
+        {
+            Instantiate(Shield, ((Vector3)Nodes[pos_node].Position) + Ajustador.transform.position, Nodes[pos_node].transform.rotation);
+            Nodes[pos_node].ToBusy();
+        }
+
+        else if(name == "Food")
+        {
+            Instantiate(Food, ((Vector3)Nodes[pos_node].Position) + Ajustador.transform.position, Nodes[pos_node].transform.rotation);
+            Nodes[pos_node].ToBusy();
         }
     }
 }
